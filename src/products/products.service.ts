0c7be63e8186a9d39cea8aedc22c045db7cc974a
segamiken <ken.s.vollyball@gmail.com>
2020-05-19 14:52:49 +0900

    Add ProductのUpdate機能

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -16,11 +16,32 @@ export class ProductsService {
         return [...this.products];
     }
 
-    getSingleProduct(ProductId: string) {
-        const product = this.products.find((prod) => prod.id === ProductId );
+    getSingleProduct(productId: string) {
+        const product = this.findProduct(productId)[0];
+        return {...product};
+    }
+
+    updateProduct(productId: string, title: string, desc: string, price: number) {
+        const [product, index] = this.findProduct(productId);
+        const updatedProduct = {...product};
+        if (title) {
+            updatedProduct.title = title;
+        }
+        if (desc) {
+            updatedProduct.description = desc;
+        }
+        if (price) {
+            updatedProduct.price = price;
+        }
+        this.products[index] = updatedProduct;
+    }
+
+    private findProduct(id: string): [Product, number] {
+        const productIndex = this.products.findIndex((prod) => prod.id === id );
+        const product = this.products[productIndex];
         if(!product) {
             throw new NotFoundException('could not find product.');
         }
-        return {...product};
+        return [product, productIndex];
     }
 }
